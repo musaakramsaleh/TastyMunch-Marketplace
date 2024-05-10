96517b107f459c9062af965360114e23b2970f65
musaakramsaleh <saleh01715972188@gmail.com>
2024-05-10 23:25:15 +0600

    ok

--- src/Pages/Home/Client.jsx
+++ src/Pages/Home/Client.jsx
@@ -4,6 +4,7 @@ import client_2 from '../../assets/client-2.png'
 import client_3 from '../../assets/client-3.webp'
 import client_4 from '../../assets/client-4.jpeg'
 import client_5 from '../../assets/client-5.png'
+import client_6 from '../../assets/client-6.png'
 const Client = () => {
     return (
         <div className='mt-16 max-w-[1440px] mx-auto'>
@@ -17,9 +18,9 @@ const Client = () => {
                 <img className='w-full border rounded-full' src={client_2} alt="" />
                 <p className='text-2xl font-lexend font-bold text-amber-400 mt-5'>Coffee Lime</p>
             </div>
-            <div className='w-[250px] h-[250px]'>
-                <img className='h-full border rounded-full' src={client_3} alt="" />
-                <p className='text-2xl font-lexend font-bold text-amber-400 mt-5'>Sultans Dine</p>
+            <div className='w-[250px]'>
+                <img className='w-full border rounded-full' src={client_6} alt="" />
+                <p className='text-2xl font-lexend font-bold text-amber-400 mt-5'>Gloria Jeans</p>
             </div>
             <div className='w-[250px]'>
                 <img className='w-full border rounded-full' src={client_4} alt="" />
